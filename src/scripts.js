0e336547ae98fce79f5221e3ec5c29249bb1ef95
Aleksandr Shablov <50119281+Protoff@users.noreply.github.com>
2020-11-18 12:57:20 +0300

    Несколько изменений

--- src/scripts.js
+++ src/scripts.js
@@ -1,72 +1,127 @@
+document.addEventListener("DOMContentLoaded", () => {
+  new SymbolsAnimate(".header .button");
 
-document.addEventListener('click', e => {
-  if ( e.target.getAttribute('href') === '#modal') {
-    e.preventDefault()
-    modal.classList.add('modal-show-animation')
-  }
-  if ( e.target.classList.contains('modal-close') || e.target.classList.contains('modal-overlay')){
-    modalClose()
-  }
-})
-
-function modalClose() {
-  if ( modal.classList.contains('modal-close-animation') || modal.classList.contains('modal-show-animation')) {
-    modal.classList.remove('modal-show-animation')
-    modal.classList.add('modal-close-animation')
-    setTimeout(() => {
-        modal.classList.remove('modal-close-animation')
-      }, 1000)
-  }
-}
-
-
-modalForm.addEventListener('submit', function(e) {
-  e.preventDefault()
-  const inputs = this.querySelectorAll('.input')
-  const vals = [...inputs].map( input => {
-    if (input.value.trim() == 0) {
-      this[input.name].style.border = '1px solid red'
-      return false
-    } else {
-      this[input.name].style.border = '1px solid #02de15'
-      return true
+  document.addEventListener("click", (e) => {
+    if (e.target.getAttribute("href") === "#modal") {
+      e.preventDefault();
+      modal.classList.add("modal-show-animation");
     }
-  })
+    if (
+      e.target.classList.contains("modal-close") ||
+      e.target.classList.contains("modal-overlay")
+    ) {
+      modalClose();
+    }
+  });
 
+  function modalClose() {
+    if (
+      modal.classList.contains("modal-close-animation") ||
+      modal.classList.contains("modal-show-animation")
+    ) {
+      modal.classList.remove("modal-show-animation");
+      modal.classList.add("modal-close-animation");
+      setTimeout(() => {
+        modal.classList.remove("modal-close-animation");
+      }, 1000);
+    }
+  }
 
-  if ( !vals.includes(false)) {
+  modalForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const inputs = this.querySelectorAll(".input");
+    const vals = [...inputs].map((input) => {
+      if (input.value.trim() == 0) {
+        this[input.name].style.border = "1px solid red";
+        return false;
+      } else {
+        this[input.name].style.border = "1px solid #02de15";
+        return true;
+      }
+    });
+
+    if (!vals.includes(false)) {
       // создаю объект данных формы
-      var elems = this.elements
+      var elems = this.elements;
       var params = [];
-      for(var i = 0; i < elems.length; i++){
-          elType = elems[i].type;
-          elName = elems[i].name;
-          if(elName){
-              params.push(elems[i].name + '=' + elems[i].value);
-          }
+      for (var i = 0; i < elems.length; i++) {
+        elType = elems[i].type;
+        elName = elems[i].name;
+        if (elName) {
+          params.push(elems[i].name + "=" + elems[i].value);
+        }
       }
       // отправляю
-      fetch('./post-server.php', {
-        method: 'POST',
+      fetch("./post-server.php", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+          "Content-Type": "application/x-www-form-urlencoded",
         },
-        body: params.join('&')
-      }).then( res => {
-        this.reset()
-        if( res.status === 200 ) {
-          modal.querySelector('.content').innerHTML = `
-          <h4 class="h4-title">Сообщение успешно отправлено и я скоро с Вами свяжусь.<br>Спасибо!</h4>`
-        } else {
-          modal.querySelector('.content').innerHTML = `
-          <h4 class="h4-title">Что-то пошло не так!<br>Пожалуйста, попробуйте позже, или напишите мне иным способом - контакты внизу страницы.<br>Спасибо!</h4>`
-        }
-        setTimeout(() => {
-          modalClose()
-        }, 10000);
-      }).catch(error => {
-        console.error('Ошибка: ', error)
+        body: params.join("&"),
       })
+        .then((res) => {
+          this.reset();
+          if (res.status === 200) {
+            modal.querySelector(".content").innerHTML = `
+          <h4 class="h4-title">Сообщение успешно отправлено и я скоро с Вами свяжусь.<br>Спасибо!</h4>`;
+          } else {
+            modal.querySelector(".content").innerHTML = `
+          <h4 class="h4-title">Что-то пошло не так!<br>Пожалуйста, попробуйте позже, или напишите мне иным способом - контакты внизу страницы.<br>Спасибо!</h4>`;
+          }
+          setTimeout(() => {
+            modalClose();
+          }, 10000);
+        })
+        .catch((error) => {
+          console.error("Ошибка: ", error);
+        });
+    }
+  });
+});
+
+class SymbolsAnimate {
+  constructor(el) {
+    this.el = el;
+    this.animateStr();
+  }
+  animateStr() {
+    //const el = document.querySelector('#footer .item[data-id=footerMail] .link')
+    const el = document.querySelector(this.el);
+    const elArr = el.textContent.split("");
+
+    const newStr = elArr
+      .map((item) => {
+        return `<span>${item}</span>`;
+      })
+      .join("");
+
+    el.innerHTML = newStr;
+
+    this.animateSymbol();
   }
 
-})
+  animateSymbol() {
+    setInterval(() => {
+      this.addClassAnima();
+    }, 300);
+  }
+
+  addClassAnima() {
+    const els = document.querySelectorAll(this.el + " span");
+    const randNum = this.randomInteger(0, els.length - 1);
+    //const els = document.querySelectorAll('#footer .item[data-id=footerMail] .link span')
+    if (els[randNum].classList.contains("symbols-animation")) {
+      this.addClassAnima();
+      return;
+    }
+    els[randNum].classList.add("symbols-animation");
+    setTimeout(() => {
+      els[randNum].classList.remove("symbols-animation");
+    }, 2000);
+  }
+
+  randomInteger(min, max) {
+    const rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+  }
+}
